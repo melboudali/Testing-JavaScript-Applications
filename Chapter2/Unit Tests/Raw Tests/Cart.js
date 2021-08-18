69b54058ee3ec716f539ef836a70ad0458123eba
MedElBoudali <med.sneaky@gmail.com>
2021-08-18 23:20:26 +0000

    -+ Row Tests.

--- Chapter2/Unit Tests/Raw Tests/Cart.js
+++ Chapter2/Unit Tests/Raw Tests/Cart.js
@@ -5,14 +5,6 @@ class Cart {
 	addToCart(item) {
 		this.items.push(item);
 	}
-	removeFromCart(item) {
-		for (let i = 0; i <= this.items.length; i++) {
-			if (this.items[i] === item) {
-				this.items.splice(i, 1);
-				return;
-			}
-		}
-	}
 }
 
 module.exports = Cart;
