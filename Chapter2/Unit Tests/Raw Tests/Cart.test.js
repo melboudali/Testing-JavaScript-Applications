002ac5a3fe4737adba486d4a0e368252b9921b11
MedElBoudali <med.sneaky@gmail.com>
2021-08-18 15:19:18 +0000

    + JEST tests.

--- Chapter2/Unit Tests/Raw Tests/Cart.test.js
+++ Chapter2/Unit Tests/Raw Tests/Cart.test.js
@@ -9,28 +9,42 @@ const hasOneItem = cart.items.length === 1;
 const hasCupCake = cart.items[0] === "Cupcake";
 
 // Test N1
-if (hasOneItem && hasCupCake) {
-	console.log("Test N1: The addToCart function can add an item to the cart");
-} else {
-	const actualContent = cart.items.join(", ");
+// if (hasOneItem && hasCupCake) {
+// 	console.log("Test N1: The addToCart function can add an item to the cart");
+// } else {
+// 	const actualContent = cart.items.join(", ");
 
-	console.error("Test N1: The addToCart function didn't do what we expect!");
-	console.error(`Test N1: Here is the actual content of the cart: ${actualContent}`);
+// 	console.error("Test N1: The addToCart function didn't do what we expect!");
+// 	console.error(`Test N1: Here is the actual content of the cart: ${actualContent}`);
 
-	throw new Error("Test N1 failed!");
-}
+// 	throw new Error("Test N1 failed!");
+// }
 
 // Test N2 using assertions
-cart.addToCart("Cheesecake");
-assert.deepStrictEqual(cart.items, ["Cupcake", "Cheesecake"]);
-console.log("Test N2: The addToCart function can add an item to the cart");
+// cart.addToCart("Cheesecake");
+// assert.deepStrictEqual(cart.items, ["Cupcake", "Cheesecake"]);
+// console.log("Test N2: The addToCart function can add an item to the cart");
 
 // Test N3
-cart.addToCart("Pastry");
-cart.addToCart("Croissant");
-cart.removeFromCart("Cheesecake");
+// cart.addToCart("Pastry");
+// cart.addToCart("Croissant");
+// cart.removeFromCart("Cheesecake");
 
-assert.deepStrictEqual(cart.items.length, 3);
-assert.deepStrictEqual(cart.items, ["Cupcake", "Pastry", "Croissant"]);
+// assert.deepStrictEqual(cart.items.length, 3);
+// assert.deepStrictEqual(cart.items, ["Cupcake", "Pastry", "Croissant"]);
+// console.log("Test N3: The removeCart function can remove an item from the cart");
 
-console.log("Test N3: The removeCart function can remove an item from the cart");
+// testing with JEST
+test("Test N2: The addToCart function can add an item to the cart", () => {
+	cart.addToCart("Cheesecake");
+	expect(cart.items).toEqual(["Cupcake", "Cheesecake"]);
+});
+
+test("The removeCart function can remove an item from the cart", () => {
+	cart.addToCart("Pastry");
+	cart.addToCart("Croissant");
+	cart.removeFromCart("Cheesecake");
+
+	expect(cart.items.length).toEqual(3);
+	expect(cart.items).toEqual(["Cupcake", "Pastry", "Croissant"]);
+});
