19dcb59b5988c273d30f0b6c9b4c46ba63b5eab9
MedElBoudali <med.sneaky@gmail.com>
2021-08-19 14:33:13 +0000

    + Async Test.

---
+++ Chapter2/Integration Tests/Async Test/Cart.test.js
@@ -0,0 +1,10 @@
+const { db, closeConnection } = require("../../../dbConnection");
+const { createCart } = require("./cart");
+
+test("createCart creates a cart for a username with async", async () => {
+	await db("carts").truncate();
+	await createCart("Moe EL BOUDALI");
+	const result = await db.select("username").from("carts");
+	expect(result).toEqual([{ username: "Moe EL BOUDALI" }]);
+	await closeConnection();
+});
