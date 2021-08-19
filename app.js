7ce7feb6d54c90a180c2648de73deff368c7fdde
MedElBoudali <med.sneaky@gmail.com>
2021-08-19 20:40:03 +0000

    + routes and resetState.

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 const koa = require("koa");
 const Router = require("@koa/router");
-const fetch = require("isomorphic-fetch");
 
 const app = new koa();
 const router = new Router();
 
-const carts = new Map();
+let carts = new Map();
 
-router.get("/carts/:username/items", ({ params: { username }, body, status }) => {
-	const cart = carts.get(username);
-	cart ? body(cart) : status(404);
+router.get("/carts/:username/items", ctx => {
+	const cart = carts.get(ctx.params.username);
+	cart ? (ctx.body = cart) : (ctx.status = 404);
 });
 
-router.post("/carts/:username/items/:item", ({ params: { username, item } }) => {
+router.post("/carts/:username/items/:item", ctx => {
+	const { username, item } = ctx.params;
 	const newItems = (carts.get(username) || []).concat(item);
 	carts.set(username, newItems);
 	ctx.body = newItems;
 });
+
+router.delete("/carts/:username/items/:item", ctx => {
+	const { username, item } = ctx.params;
+	const newItems = (carts.get(username) || []).filter(i => i !== item);
+	carts.set(username, newItems);
+	ctx.body = newItems;
+});
+
+app.use(router.routes());
+
+const resetState = () => {
+	carts = new Map();
+};
+
+module.exports = { app: app.listen(3000), resetState };
