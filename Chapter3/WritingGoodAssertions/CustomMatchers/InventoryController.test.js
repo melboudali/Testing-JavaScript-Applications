b327f93a571e968b8a2bd2748409dde30effc606
MedElBoudali <med.sneaky@gmail.com>
2021-08-23 21:36:06 +0000

    - files.

--- Chapter3/WritingGoodAssertions/CustomMatchers/InventoryController.test.js
+++
@@ -1,8 +0,0 @@
-const { inventory, getInventory } = require("./InventoryController");
-
-test("generatedAt in the past", () => {
-	const result = getInventory();
-
-	const currentTime = new Date(Date.now() + 1);
-	expect(result.generatedAt).toBeBefore(currentTime);
-});
