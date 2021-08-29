ea90401ea2acfe6611b3ce21503bc372f3f4deb5
MedElBoudali <med.sneaky@gmail.com>
2021-08-29 22:32:09 +0000

    + cfg

--- dbConnection.js
+++ dbConnection.js
@@ -1,4 +1,8 @@
-const db = require("knex")(require("./knexfile").development);
+const environmentName = process.env.NODE_ENV;
+const db = require("knex")(require("./knexfile")[environmentName]);
+
+// we use NODE_ENV if development mode we will use development DB else jest will use test mode with test db
+// to run migration on test db use: ./node_modules/.bin/knex migrate:latest --env test
 
 const closeConnection = () => db.destroy();
 
