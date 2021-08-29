ec7b6acfb492ae7c9fa514b78e1e50c1d1ae3dd0
MedElBoudali <med.sneaky@gmail.com>
2021-08-29 22:06:03 +0000

    + Jest with hooks.

--- Chapter2/3. Integration Tests/3. Jest with hooks/Cart.test.js
+++ Chapter2/3. Integration Tests/3. Jest with hooks/Cart.test.js
@@ -1,20 +1,15 @@
 const { db, closeConnection } = require("../../../dbConnection");
 const { createCart, addItem } = require("./cart");
 
+afterAll(async () => await closeConnection());
 test("addItem adds an item to a cart", async () => {
-	beforeAll(async () => {
-		await db("carts").truncate();
-		await db("carts_items").truncate();
-	});
-	afterAll(async () => await closeConnection());
-
 	const username = "Moe EL BOUDALI";
 	await createCart(username);
 
 	const { id: cartId } = await db.select().from("carts").where({ username });
 	await addItem(cartId, "cupcake");
 
-	const result = await db.select("itemName").from("carts_items");
+	const result = await db.select("itemName").from("carts_items_one");
 
 	expect(result).toEqual([{ cartId, itemName: "cupcake" }]);
 	await closeConnection();
