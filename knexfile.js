a09a271a5aaa8e4c8aa7470561a21800226924ad
MedElBoudali <med.sneaky@gmail.com>
2021-08-29 23:07:48 +0000

    + knex cfg

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,9 @@
 module.exports = {
+	test: {
+		client: "sqlite3",
+		connection: { filename: "./test.sqlite" },
+		useNullAsDefault: true
+	},
 	development: {
 		client: "sqlite3",
 		connection: { filename: "./dev.sqlite" },
