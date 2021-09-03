7d80d42f1ac548bcce6486f193ae0774b0de2ee0
MedElBoudali <med.sneaky@gmail.com>
2021-09-03 22:25:39 +0000

    -+ jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-	testEnvironment: "node",
+	// testEnvironment: "node",
+	testEnvironment: "jsdom",
 	globalSetup: "<rootDir>/migrateDatabases.js",
 	setupFilesAfterEnv: ["jest-extended", "<rootDir>/truncateTables.js", "<rootDir>/seedUser.js", "<rootDir>/disconnectFromDb.js"]
 };
