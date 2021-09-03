1924c4cfd00b47590494f2f66b64cd765ea4d5cc
MedElBoudali <med.sneaky@gmail.com>
2021-09-03 22:28:06 +0000

    + main.js

---
+++ Chapter6/1. Jsdom Introducing/1. Pure Html/main.js
@@ -0,0 +1,9 @@
+let count = 0;
+
+const incrementCount = () => {
+	count++;
+	window.document.getElementById("count").innerHTML = count;
+};
+
+const incrementButton = window.document.getElementById("increment-button");
+incrementButton.addEventListener("click", incrementCount);
