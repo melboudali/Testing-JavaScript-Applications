3e7544b30ff1db6a4dcff40f0afab39da4a7e2c8
MedElBoudali <med.sneaky@gmail.com>
2021-09-04 23:01:22 +0000

    -+ main.test.js

--- Chapter6/1. Jsdom Introducing/3. Jest Jsdom/main.test.js
+++ Chapter6/1. Jsdom Introducing/3. Jest Jsdom/main.test.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
-window.document.body.innerHTML = fs.readFileSync("./index.html");
+document.body.innerHTML = fs.readFileSync(`${__dirname}/index.html`);
 
 const { count, incrementCount } = require("./main");
 
 describe("incrementCount", () => {
 	test("incrementing the count", () => {
-		expect(count).toBe(0);
+		count = 0;
 		incrementCount();
 		expect(count).toBe(1);
 	});
