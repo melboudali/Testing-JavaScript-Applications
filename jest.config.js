29eafccff84682dd36ea7d1ef98e31e2fc09bd1f
MedElBoudali <med.sneaky@gmail.com>
2021-09-06 22:02:54 +0000

    -+ jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,11 @@ module.exports = {
 	// testEnvironment: "node",
 	testEnvironment: "jsdom",
 	globalSetup: "<rootDir>/migrateDatabases.js",
-	setupFilesAfterEnv: ["jest-extended", "<rootDir>/truncateTables.js", "<rootDir>/seedUser.js", "<rootDir>/disconnectFromDb.js"]
+	setupFilesAfterEnv: [
+		"<rootDir>/Chapter6/2. Asserting On The Dom/5. Writing Better Dom Assertions/setupJestDom.js",
+		"jest-extended",
+		"<rootDir>/truncateTables.js",
+		"<rootDir>/seedUser.js",
+		"<rootDir>/disconnectFromDb.js"
+	]
 };
