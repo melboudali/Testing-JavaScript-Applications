3751f9da9ecc351dd14d3ae91eeaa12af36f8741
MedElBoudali <med.sneaky@gmail.com>
2021-09-07 22:52:39 +0000

    + main

---
+++ Chapter6/3. Handling Events/3. Dom Testing Library Events/main.js
@@ -0,0 +1,14 @@
+const { addItem, data } = require("./inventoryController");
+const { updateItemList, handleAddItem, checkFormValues } = require("./domController");
+
+addItem("cheesecake", 3);
+addItem("apple pie", 8);
+addItem("carrot cake", 7);
+
+updateItemList(data.inventory);
+
+const form = document.getElementById("add-item-form");
+form.addEventListener("submit", handleAddItem);
+form.addEventListener("input", checkFormValues);
+
+checkFormValues();
