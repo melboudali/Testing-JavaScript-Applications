fbef0cef748364d88197be98e4324936497b9d45
MedElBoudali <med.sneaky@gmail.com>
2021-09-07 22:41:55 +0000

    + main.test

---
+++ Chapter6/3. Handling Events/1. Handling Raw Events/main.test.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const initialHtml = fs.readFileSync(`${__dirname}/index.html`);
+const { screen, getByText } = require("@testing-library/dom");
+
+beforeEach(() => {
+	document.body.innerHTML = initialHtml;
+
+	jest.resetModules();
+	require("./main");
+});
+
+test("adding items through the form", () => {
+	screen.getByPlaceholderText("Item name").value = "cupcake";
+	screen.getByPlaceholderText("Quantity").value = "7";
+
+	const event = new Event("submit");
+	const form = document.getElementById("add-item-form");
+	form.dispatchEvent(event);
+
+	const itemList = document.getElementById("item-list");
+	expect(getByText(itemList, "cupcake - Quantity: 7")).toBeInTheDocument();
+});
+
+describe("item name validation", () => {
+	test("entering valid item names ", () => {
+		const itemField = screen.getByPlaceholderText("Item name");
+		itemField.value = "cheesecake";
+		const inputEvent = new Event("input");
+
+		itemField.dispatchEvent(inputEvent);
+
+		expect(screen.getByText("cheesecake is valid!")).toBeInTheDocument();
+	});
+
+	test("entering invalid item names ", () => {
+		const itemField = screen.getByPlaceholderText("Item name");
+		itemField.value = "cupcake";
+		const inputEvent = new Event("input");
+
+		itemField.dispatchEvent(inputEvent);
+
+		expect(screen.getByText("cupcake is not a valid item.")).toBeInTheDocument();
+	});
+});
