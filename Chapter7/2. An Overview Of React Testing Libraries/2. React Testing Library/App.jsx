2ac6787e4cd37966eb9afe71112159443766490e
MedElBoudali <med.sneaky@gmail.com>
2021-09-12 22:16:54 +0000

    + App.jsx

---
+++ Chapter7/2. An Overview Of React Testing Libraries/2. React Testing Library/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ItemForm from "./ItemForm.jsx";
+
+const App = () => {
+	return (
+		<div>
+			<h1>Inventory Contents</h1>
+			<ItemForm />
+		</div>
+	);
+};
+export default App;
