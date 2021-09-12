e0d029256235b0cd23e995bd22f6b114b5b221fb
MedElBoudali <med.sneaky@gmail.com>
2021-09-12 22:20:24 +0000

    + ItemForm.jsx

---
+++ Chapter7/2. An Overview Of React Testing Libraries/2. React Testing Library/ItemForm.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { API_ADDR } from "./constants";
+
+const addItemRequest = (itemName, quantity) => {
+	fetch(`${API_ADDR}/inventory/${itemName}`, {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ quantity })
+	});
+};
+
+const ItemForm = () => {
+	const [itemName, setItemName] = useState("");
+	const [quantity, setQuantity] = useState(0);
+
+	const onSubmit = e => {
+		e.preventDefault();
+		addItemRequest(itemName, quantity);
+	};
+
+	return (
+		<form onSubmit={onSubmit}>
+			<input onChange={e => setItemName(e.target.value)} placeholder="Item name" />
+			<input onChange={e => setQuantity(parseInt(e.target.value, 10))} placeholder="Quantity" />
+			<button type="submit">Add item</button>
+		</form>
+	);
+};
+
+export default ItemForm;
