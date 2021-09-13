68f63e0f5d4e016bf882afac9be24f6cb542cd1c
MedElBoudali <med.sneaky@gmail.com>
2021-09-13 20:58:52 +0000

    -+ App.test

--- Chapter7/2. An Overview Of React Testing Libraries/2. React Testing Library/App.test.jsx
+++ Chapter7/2. An Overview Of React Testing Libraries/2. React Testing Library/App.test.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import App from "./App.jsx";
 import { render } from "@testing-library/react";
+import nock from "nock";
+
+beforeEach(() => {
+	nock(API_ADDR).get("/inventory").reply(200, { cheesecake: 2, croissant: 5, macaroon: 96 });
+});
+
+afterEach(() => {
+	if (!nock.isDone()) {
+		nock.cleanAll();
+		throw new Error("Not all mocked endpoints received requests.");
+	}
+});
 
 test("renders the appropriate header", () => {
 	const { getByText } = render(<App />);
