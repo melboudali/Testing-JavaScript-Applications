0bb9023e40b1db1273f580f1d7ccc9367f0ccdf1
MedElBoudali <med.sneaky@gmail.com>
2021-09-17 21:30:58 +0000

    + ItemList.test.jsx

---
+++ Chapter8/2. Snapshot Testing/1. Component Snapshots/ItemList.test.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ItemList, { generateItemText } from "./ItemList.jsx";
+import { render } from "@testing-library/react";
+
+// jest.mock("react-spring/renderprops");
+
+describe("ItemList Component", () => {
+	test("check items list", () => {
+		const Items = { cheesecake: 2, croissant: 5, macaroon: 96 };
+		const { getByText } = render(<ItemList items={Items} />);
+
+		const listElement = document.querySelector("ul");
+		expect(listElement.childElementCount).toBe(3);
+		expect(getByText(generateItemText("cheesecake", 2))).toBeInTheDocument();
+		expect(getByText(generateItemText("croissant", 5))).toBeInTheDocument();
+		expect(getByText(generateItemText("macaroon", 96))).toBeInTheDocument();
+	});
+});
+
+describe("generateItemText", () => {
+	test("generating an item's text", () => {
+		expect(generateItemText("cheesecake", 3)).toBe("cheesecake - Quantity: 3");
+		expect(generateItemText("apple pie", 22)).toBe("apple pie - Quantity: 22");
+	});
+});
