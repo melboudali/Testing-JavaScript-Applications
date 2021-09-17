28b2c5a518276f40e10e88061751beb928f1b820
MedElBoudali <med.sneaky@gmail.com>
2021-09-17 21:36:49 +0000

    + App.jsx

--- Chapter7/2. An Overview Of React Testing Libraries/2. React Testing Library/App.jsx
+++ Chapter7/2. An Overview Of React Testing Libraries/2. React Testing Library/App.jsx
@@ -1,16 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { API_ADDR } from "./constants.js";
 import ItemForm from "./ItemForm.jsx";
 import ItemList from "./ItemList.jsx";
 
 const App = () => {
 	const [items, setItems] = useState({});
+	const isMounted = useRef(null);
 	useEffect(() => {
+		isMounted.current = true;
 		const loadItems = async () => {
 			const response = await fetch(`${API_ADDR}/inventory`);
-			setItems(await response.json());
+			const responseBody = await response.json();
+			if (isMounted.current) setItems(responseBody);
 		};
 		loadItems();
+		return () => (isMounted.current = false);
 	}, []);
 
 	return (
