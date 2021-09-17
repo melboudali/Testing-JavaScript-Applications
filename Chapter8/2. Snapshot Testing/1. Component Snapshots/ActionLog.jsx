cfe35f70462ab8fbf5fa8b3ad71bae09f0d317b2
MedElBoudali <med.sneaky@gmail.com>
2021-09-17 21:16:52 +0000

    + ActionLog

---
+++ Chapter8/2. Snapshot Testing/1. Component Snapshots/ActionLog.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ActionLog = ({ actions }) => {
+	return (
+		<div data-testid="action-log">
+			<h2>Action Log</h2>
+			<ul>
+				{actions.map(({ time, message, data }, i) => {
+					const date = new Date(time).toUTCString();
+					return (
+						<li key={i}>
+							Date: {date} - Message: {message} - Data: {JSON.stringify(data)}
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
+};
+export default ActionLog;
