388fcd33c6ec02e9acbb699d913696807416a94a
MedElBoudali <med.sneaky@gmail.com>
2021-09-18 22:33:00 +0000

    + ItemList test

---
+++ Chapter8/3. Testing Styles/1. css_classes/ItemList.test.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import ItemList, { generateItemText } from "./ItemList.jsx";
+import { render } from "@testing-library/react";
+
+describe("ItemList Component", () => {
+	test("check items list", () => {
+		const Items = { cheesecake: 2, croissant: 5, macaroon: 96 };
+		const { getByText } = render(<ItemList items={Items} />);
+
+		const listElement = document.querySelector("ul");
+		expect(listElement.childElementCount).toBe(3);
+		expect(getByText(generateItemText("cheesecake", 2))).toBeInTheDocument();
+		expect(getByText(generateItemText("croissant", 5))).toBeInTheDocument();
+		expect(getByText(generateItemText("macaroon", 96))).toBeInTheDocument();
+	});
+
+	test("highlighting items that are almost out of stock", () => {
+		const Items = { cheesecake: 2, croissant: 5, macaroon: 96 };
+
+		const { getByText } = render(<ItemList items={Items} />);
+		const cheesecakeItem = getByText(generateItemText("cheesecake", 2));
+		const macaroon = getByText(generateItemText("macaroon", 96));
+		// expect(cheesecakeItem).toHaveClass("almost-out-of-stock");
+		// expect(cheesecakeItem).toHaveStyle({ color: "red" });
+		expect(cheesecakeItem).toHaveStyleRule("font-weight", "bold");
+		expect(cheesecakeItem).toHaveStyleRule("color", "red");
+
+		expect(macaroon).toHaveStyleRule("color", "black");
+		expect(macaroon).toHaveStyleRule("font-weight", "normal");
+	});
+
+	test("highlighting items that are almost out of stock with snapshot", () => {
+		const Items = { cheesecake: 2, croissant: 5, macaroon: 96 };
+
+		const { getByText } = render(<ItemList items={Items} />);
+		const cheesecakeItem = getByText(generateItemText("cheesecake", 2));
+		const macaroon = getByText(generateItemText("macaroon", 96));
+		expect(cheesecakeItem).toMatchSnapshot();
+		expect(macaroon).toMatchSnapshot();
+	});
+});
+
+describe("generateItemText", () => {
+	test("generating an item's text", () => {
+		expect(generateItemText("cheesecake", 3)).toBe("cheesecake - Quantity: 3");
+		expect(generateItemText("apple pie", 22)).toBe("apple pie - Quantity: 22");
+	});
+});
