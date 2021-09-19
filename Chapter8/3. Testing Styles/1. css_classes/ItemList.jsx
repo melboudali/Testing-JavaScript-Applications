8903bf6206c76c24000a7cf80ce8544d9b2ec4ec
MedElBoudali <med.sneaky@gmail.com>
2021-09-19 23:31:13 +0000

    -+ ItemList

--- Chapter8/3. Testing Styles/1. css_classes/ItemList.jsx
+++ Chapter8/3. Testing Styles/1. css_classes/ItemList.jsx
@@ -1,30 +1,13 @@
 import React from "react";
-import styled, { css, keyframes } from "styled-components";
-
-const pulsate = keyframes`
-	0% { opacity: .3; }
-	50% { opacity: 1; }
-	100% { opacity: .3; }
-`;
-
-const ListItem = styled.li`
-	font-weight: ${({ quantity }) => (quantity < 5 ? "bold" : "normal")};
-	color: ${({ quantity }) => (quantity < 5 ? "red" : "black")};
-	${({ quantity }) =>
-		quantity < 5 &&
-		css`
-			animation: ${pulsate} 2s infinite;
-		`}
-`;
 
 const ItemList = ({ items }) => {
 	const itemsList = Object.entries(items);
 	return (
 		<ul>
 			{itemsList.map(([itemName, quantity]) => (
-				<ListItem key={itemName} quantity={quantity}>
+				<li key={itemName} className={quantity < 5 ? "almost-out-of-stock" : null}>
 					{generateItemText(itemName, quantity)}
-				</ListItem>
+				</li>
 			))}
 		</ul>
 	);
