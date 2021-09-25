26c825b72f319398fc6c2e02fdea90432f69335d
MedElBoudali <med.sneaky@gmail.com>
2021-09-25 21:26:26 +0000

    -+ file renamed

--- Chapter9/2. Writing A js_module Using Tdd/3. Creating Report/inventoryReport.spec.js
+++ Chapter9/2. Writing A js_module Using Tdd/3. Creating Report/inventoryReport.spec.js
@@ -2,25 +2,25 @@ const fs = require("fs");
 const { generateItemRow, generateTotalRow, createInventoryValuesReport } = require("./inventoryReport");
 
 describe("generateItemRow", () => {
-	test("generating an item's row", () => {
+	it("generate an item's row", () => {
 		expect(generateItemRow({ name: "macaroon", quantity: 12, price: 3 })).toBe("macaroon,12,3,36");
 		expect(generateItemRow({ name: "cheesecake", quantity: 6, price: 12 })).toBe("cheesecake,6,12,72");
 		expect(generateItemRow({ name: "apple pie", quantity: 5, price: 15 })).toBe("apple pie,5,15,75");
 	});
 
-	test("committing soldout items", () => {
+	it("commit soldout items", () => {
 		expect(generateItemRow({ name: "macaroon", quantity: 0, price: 3 })).toBe(null);
 		expect(generateItemRow({ name: "cheesecake", quantity: 0, price: 12 })).toBe(null);
 	});
 
-	test("committing free items", () => {
+	it("commit free items", () => {
 		expect(generateItemRow({ name: "plastic cups", quantity: 99, price: 0 })).toBe(null);
 		expect(generateItemRow({ name: "napkins", quantity: 200, price: 0 })).toBe(null);
 	});
 });
 
 describe("generateTotalRow", () => {
-	test("generating a total row", () => {
+	it("generate a total row", () => {
 		const items = [
 			{ name: "apple pie", quantity: 3, price: 15 },
 			{ name: "plastic cups", quantity: 0, price: 55 },
@@ -35,7 +35,7 @@ describe("generateTotalRow", () => {
 });
 
 describe("createInventoryValuesReport", () => {
-	test("creating reports", () => {
+	it("create reports", () => {
 		const items = [
 			{ name: "apple pie", quantity: 3, price: 15 },
 			{ name: "cheesecake", quantity: 2, price: 12 },
